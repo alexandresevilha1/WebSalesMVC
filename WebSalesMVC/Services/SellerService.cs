59cfab9a675d3e3241e95d4ce57fdeb493a7edce
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2024-03-01 09:17:36 -0300

    Seller Details e Eager Loading

--- WebSalesMVC/Services/SellerService.cs
+++ WebSalesMVC/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using WebSalesMVC.Data;
 using WebSalesMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebSalesMVC.Services
 {
@@ -25,7 +26,7 @@ namespace WebSalesMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
