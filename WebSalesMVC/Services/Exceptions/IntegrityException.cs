92793eb287ebdece7bc15c1d5a6050143df5e6d2
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2024-03-02 09:20:19 -0300

    Exception Handling for delete (referential integrity)

---
+++ WebSalesMVC/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSalesMVC.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message) 
+        { 
+        }
+    }
+}
